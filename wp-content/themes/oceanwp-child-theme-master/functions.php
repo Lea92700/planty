7126ba6757be004bc6084682a0589e5ddf9db7aa
Léa Germany <lea.germany@outlook.fr>
2023-07-16 01:45:29 +0200

    premier commit

---
+++ wp-content/themes/oceanwp-child-theme-master/functions.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * OceanWP Child Theme Functions
+ *
+ * When running a child theme (see http://codex.wordpress.org/Theme_Development
+ * and http://codex.wordpress.org/Child_Themes), you can override certain
+ * functions (those wrapped in a function_exists() call) by defining them first
+ * in your child theme's functions.php file. The child theme's functions.php
+ * file is included before the parent theme's file, so the child theme
+ * functions will be used.
+ *
+ * Text Domain: oceanwp
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ */
+
+/**
+ * Load the parent style.css file
+ *
+ * @link http://codex.wordpress.org/Child_Themes
+ */
+function oceanwp_child_enqueue_parent_style() {
+
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update the theme).
+	$theme   = wp_get_theme( 'OceanWP' );
+	$version = $theme->get( 'Version' );
+
+	// Load the stylesheet.
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
+function afficher_lien_admin_si_connecte($nav_menu, $args) {
+    if (is_user_logged_in()) {
+        $admin_url = admin_url();
+       $nav_menu.= '<li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a href="' . $admin_url . '">Admin</a></li>';
+    }
+	return $nav_menu;
+}
+add_action('wp_nav_menu_items', 'afficher_lien_admin_si_connecte',10,2);
